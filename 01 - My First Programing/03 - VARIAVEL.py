65ce1ad9cc5138c5f15a1d23d10f2452023159ad
Enzo <enzo.eng_97@hotmail.com>
2022-12-04 23:25:16 -0300

    My First Programing

---
+++ 01 - My First Programing/03 - VARIAVEL.py
@@ -0,0 +1,15 @@
+
+faturamento = 1500
+custo = 800
+lucro = faturamento - custo 
+print(faturamento)
+print(lucro)
+
+"""### Pegar informações do Usuário
+
+variavel = input('Texto para o Usuário')
+"""
+
+#para cadastrar um cliente
+cpf = input('Digite seu cpf (apenas números, sem pontos e traços)')
+print('O cpf do cliente é ' + cpf)
